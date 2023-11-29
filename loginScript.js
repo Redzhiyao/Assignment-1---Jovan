ddc6dc9ba94f78e8cbce8715840a6dcd384fa827
J-v-T <s10259920@connect.np.edu.sg>
2023-11-30 01:08:58 +0800

    Completion of signUp design and others
    Faulty javascript code in loginScript that is meant to remove sign up
    button from index after logging in. To be fixed in the future.
    Addition of files such as loginScript, signUp and login.
    Full general design of signup page is complete, minor changes to be
    made in the future.

---
+++ loginScript.js
@@ -0,0 +1,41 @@
+const forms = document.querySelector(".forms"),
+pwShowHide = document.querySelectorAll(".eye-icon"),
+links = document.querySelectorAll(".link");
+
+pwShowHide.forEach((eyeIcon) => {
+eyeIcon.addEventListener("click", () => {
+let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+
+    pwFields.forEach((password) => {
+      if (password.type === "password") {
+        password.type = "text";
+        eyeIcon.classList.replace("bx-hide", "bx-show");
+        return;
+      }
+      password.type = "password";
+      eyeIcon.classList.replace("bx-show", "bx-hide");
+    });
+  });
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    forms.classList.toggle("show-signup");
+  });
+});
+
+const signupButton = document.getElementById('cta');
+const mainSignupButton = document.getElementById('mainSignupButton');
+const mainLoginButton = document.getElementById('mainLoginButton');
+
+function removeSignupButton() {
+    cta.style.display = 'none';
+}
+
+mainSignupButton.addEventListener('click', () => {
+    removeSignupButton();
+});
+
+mainLoginButton.addEventListener('click', () => {
+    removeSignupButton();
+});
