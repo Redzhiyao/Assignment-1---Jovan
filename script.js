86fbdc3491872bba02637966ee6e9138974c0c47
J-v-T <s10259920@connect.np.edu.sg>
2023-12-01 03:14:08 +0800

    Minor changes to every page + cart page
    Addition of new thankyoupage, which is seen after user purchases items.
    Cart page has been made fully, further changes to be made in the future.
    Addition of images for shop and categories page.
    Addition of non functional javascript code.

--- script.js
+++ script.js
@@ -10,3 +10,45 @@ menuOpen.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   overlay.classList.remove("overlay--active");
 });
+
+// cart js
+
+function addToCart(itemId) {
+  const newItem = {
+    id: itemId,
+    name: 'Sample Item',
+    price: 19.99,
+    quantity: 1 // Assuming initial quantity is 1
+  };
+
+  let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+  const existingItem = cart.find(item => item.id === newItem.id);
+
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.push(newItem);
+  }
+
+  localStorage.setItem('cart', JSON.stringify(cart));
+
+  updateCartUI(); // Update the cart UI
+  alert('Item added to cart!');
+}
+
+// Function to update the cart UI
+function updateCartUI() {
+  const cartContainer = document.getElementById('cart-container');
+}
+
+document.getElementById('checkout-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  const itemId = 'item-123'; // Replace this with your item ID
+  addToCart(itemId);
+});
+
+window.addEventListener('load', function() {
+  updateCartUI(); // Update the cart UI initially
+});
